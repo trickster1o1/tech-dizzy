40839295a50486d742c75d52e1feb69d2d387a39
trickster1o1 <nischaltuladhar15@gmail.com>
2023-08-20 14:12:33 +0545

    overlay fix

--- resources/views/Frontend/home/components/projects.blade.php
+++ resources/views/Frontend/home/components/projects.blade.php
@@ -20,7 +20,7 @@
             </div>
             <div>
                 <div
-                    style="background-image: url('https://images.unsplash.com/photo-1601784551446-20c9e07cdbdb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bW9iaWxlJTIwcGhvbmV8ZW58MHx8MHx8fDA%3D&w=1000&q=80');">
+                    style="background-image: url('https://cdn.pixabay.com/photo/2017/07/15/19/42/train-track-2507499_640.jpg');">
                     <div class="overlay">
 
                         <div>
@@ -36,7 +36,7 @@
             </div>
             <div>
                 <div
-                    style="background-image: url('https://images.unsplash.com/photo-1601784551446-20c9e07cdbdb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bW9iaWxlJTIwcGhvbmV8ZW58MHx8MHx8fDA%3D&w=1000&q=80');">
+                    style="background-image: url('https://www.zdnet.com/a/img/resize/a599efb452885d4f668855aea18aae668867a960/2023/02/06/10e9ac75-29ce-4e6c-b2fb-8df48e1e40b0/oneplus-11-never-settle.jpg?auto=webp&fit=crop&height=900&width=1200');">
                     <div class="overlay">
 
                         <div>
