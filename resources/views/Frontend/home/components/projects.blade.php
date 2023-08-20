564f5f034c2e45892c82ca692a20e9befe409e17
trickster1o1 <nischaltuladhar15@gmail.com>
2023-08-20 13:22:42 +0545

    global variables init

--- resources/views/Frontend/home/components/projects.blade.php
+++ resources/views/Frontend/home/components/projects.blade.php
@@ -3,35 +3,50 @@
         <h1>Our Projects</h1>
         <div class="project-card">
             <div>
-                <div>
-                    <div>
-                        <h4>Sangaii</h4>
-                        <p>
-                            Our system is very easy to use both for riders and passengers. Riders can publish their ride
-                            details and passengers can choose and book their .....
-                        </p>
+                <div
+                    style="background-image: url('https://images.unsplash.com/photo-1601784551446-20c9e07cdbdb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bW9iaWxlJTIwcGhvbmV8ZW58MHx8MHx8fDA%3D&w=1000&q=80');">
+                    <div class="overlay">
+
+                        <div>
+                            <h4>Sangaii</h4>
+                            <p>
+                                Our system is very easy to use both for riders and passengers. Riders can publish their
+                                ride
+                                details and passengers can choose and book their .....
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
             <div>
-                <div>
-                    <div>
-                        <h4>Sangaii</h4>
-                        <p>
-                            Our system is very easy to use both for riders and passengers. Riders can publish their ride
-                            details and passengers can choose and book their .....
-                        </p>
+                <div
+                    style="background-image: url('https://images.unsplash.com/photo-1601784551446-20c9e07cdbdb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bW9iaWxlJTIwcGhvbmV8ZW58MHx8MHx8fDA%3D&w=1000&q=80');">
+                    <div class="overlay">
+
+                        <div>
+                            <h4>Sangaii</h4>
+                            <p>
+                                Our system is very easy to use both for riders and passengers. Riders can publish their
+                                ride
+                                details and passengers can choose and book their .....
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
             <div>
-                <div>
-                    <div>
-                        <h4>Sangaii</h4>
-                        <p>
-                            Our system is very easy to use both for riders and passengers. Riders can publish their ride
-                            details and passengers can choose and book their .....
-                        </p>
+                <div
+                    style="background-image: url('https://images.unsplash.com/photo-1601784551446-20c9e07cdbdb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bW9iaWxlJTIwcGhvbmV8ZW58MHx8MHx8fDA%3D&w=1000&q=80');">
+                    <div class="overlay">
+
+                        <div>
+                            <h4>Sangaii</h4>
+                            <p>
+                                Our system is very easy to use both for riders and passengers. Riders can publish their
+                                ride
+                                details and passengers can choose and book their .....
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
